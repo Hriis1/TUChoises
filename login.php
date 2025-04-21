fe59e489b57308dcc899e83e299f0b5272e07b53
hrishd <hrishd@abv.bg>
2025-04-21 23:07:55 +0300

    login page stuff

--- login.php
+++ login.php
@@ -34,17 +34,16 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-l-50 p-r-50 p-t-77 p-b-30">
-				<form class="login100-form validate-form">
+				<form method="POST" action="backend/authenticate.php" class="login100-form validate-form">
 					<span class="login100-form-title p-b-55">
 						Login
 					</span>
 
-					<div class="wrap-input100 validate-input m-b-16"
-						data-validate="Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
+					<div class="wrap-input100 validate-input m-b-16">
+						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
-							<span class="lnr lnr-envelope"></span>
+							<i class="fa-regular fa-user" style="font-size: 22px; margin-top: 4px; margin-left: 1px"></i>
 						</span>
 					</div>
 
